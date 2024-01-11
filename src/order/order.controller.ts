02959d574cc0ac154b7115b35d1ab45a88b06dca
hardikshakya <hardikshakya1997@gmail.com>
2024-01-11 09:47:23 +0530

    feat(order): added order module to register order microservice to communicate

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Inject,
+  Post,
+  OnModuleInit,
+  UseGuards,
+  Req,
+} from '@nestjs/common';
+import { ClientGrpc } from '@nestjs/microservices';
+import { Observable } from 'rxjs';
+
+import {
+  CreateOrderResponse,
+  OrderServiceClient,
+  ORDER_SERVICE_NAME,
+  CreateOrderRequest,
+} from './order.pb';
+import { AuthGuard } from '../auth/auth.guard';
+import { RequestWithUser } from 'src/requestWithUser.interface';
 
 @Controller('order')
-export class OrderController {}
+export class OrderController implements OnModuleInit {
+  private svc: OrderServiceClient;
+
+  @Inject(ORDER_SERVICE_NAME)
+  private readonly client: ClientGrpc;
+
+  public onModuleInit(): void {
+    this.svc = this.client.getService<OrderServiceClient>(ORDER_SERVICE_NAME);
+  }
+
+  @Post()
+  @UseGuards(AuthGuard)
+  private async createOrder(
+    @Req() req: RequestWithUser,
+  ): Promise<Observable<CreateOrderResponse>> {
+    const body: CreateOrderRequest = req.body;
+
+    body.userId = <number>req.user;
+
+    return this.svc.createOrder(body);
+  }
+}
--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -1,7 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+
 import { OrderController } from './order.controller';
+import { ORDER_SERVICE_NAME, ORDER_PACKAGE_NAME } from './order.pb';
 
 @Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: ORDER_SERVICE_NAME,
+        transport: Transport.GRPC,
+        options: {
+          url: '0.0.0.0:50052',
+          package: ORDER_PACKAGE_NAME,
+          protoPath: 'node_modules/quickshop-shared-proto/proto/order.proto',
+        },
+      },
+    ]),
+  ],
   controllers: [OrderController],
 })
 export class OrderModule {}
