691741e09322d38297937c5d4182a18a83c80ded
hardikshakya <hardikshakya1997@gmail.com>
2024-01-11 09:49:40 +0530

    feat(product): added product module

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,51 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Inject,
+  OnModuleInit,
+  Param,
+  ParseIntPipe,
+  UseGuards,
+  Post,
+  Body,
+} from '@nestjs/common';
+import { ClientGrpc } from '@nestjs/microservices';
+import { Observable } from 'rxjs';
+
+import {
+  FindOneResponse,
+  ProductServiceClient,
+  PRODUCT_SERVICE_NAME,
+  CreateProductResponse,
+  CreateProductRequest,
+} from './product.pb';
+import { AuthGuard } from '../auth/auth.guard';
 
 @Controller('product')
-export class ProductController {}
+export class ProductController implements OnModuleInit {
+  private svc: ProductServiceClient;
+
+  @Inject(PRODUCT_SERVICE_NAME)
+  private readonly client: ClientGrpc;
+
+  public onModuleInit(): void {
+    this.svc =
+      this.client.getService<ProductServiceClient>(PRODUCT_SERVICE_NAME);
+  }
+
+  @Post()
+  @UseGuards(AuthGuard)
+  private async createProduct(
+    @Body() body: CreateProductRequest,
+  ): Promise<Observable<CreateProductResponse>> {
+    return this.svc.createProduct(body);
+  }
+
+  @Get(':id')
+  @UseGuards(AuthGuard)
+  private async findOne(
+    @Param('id', ParseIntPipe) id: number,
+  ): Promise<Observable<FindOneResponse>> {
+    return this.svc.findOne({ id });
+  }
+}
--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,7 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+
 import { ProductController } from './product.controller';
+import { PRODUCT_PACKAGE_NAME, PRODUCT_SERVICE_NAME } from './product.pb';
 
 @Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: PRODUCT_SERVICE_NAME,
+        transport: Transport.GRPC,
+        options: {
+          url: '0.0.0.0:50053',
+          package: PRODUCT_PACKAGE_NAME,
+          protoPath: 'node_modules/quickshop-shared-proto/proto/product.proto',
+        },
+      },
+    ]),
+  ],
   controllers: [ProductController],
 })
 export class ProductModule {}
