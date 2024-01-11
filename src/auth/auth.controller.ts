bf1e6dae53dc786a029f4f1daa696bc43c2c4eb3
hardikshakya <hardikshakya1997@gmail.com>
2024-01-11 09:45:53 +0530

    feat(auth): added auth module which will also able to requests to our authentication microservice

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,45 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Inject,
+  OnModuleInit,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { ClientGrpc } from '@nestjs/microservices';
+import { Observable } from 'rxjs';
+
+import {
+  AUTH_SERVICE_NAME,
+  AuthServiceClient,
+  LoginRequest,
+  LoginResponse,
+  RegisterRequest,
+  RegisterResponse,
+} from './auth.pb';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController implements OnModuleInit {
+  private svc: AuthServiceClient;
+
+  @Inject(AUTH_SERVICE_NAME)
+  private readonly client: ClientGrpc;
+
+  public onModuleInit(): void {
+    this.svc = this.client.getService<AuthServiceClient>(AUTH_SERVICE_NAME);
+  }
+
+  @Post('register')
+  private async register(
+    @Body() body: RegisterRequest,
+  ): Promise<Observable<RegisterResponse>> {
+    return this.svc.register(body);
+  }
+
+  @Put('login')
+  private async login(
+    @Body() body: LoginRequest,
+  ): Promise<Observable<LoginResponse>> {
+    return this.svc.login(body);
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientGrpc } from '@nestjs/microservices';
+import { firstValueFrom } from 'rxjs';
+
+import {
+  AUTH_SERVICE_NAME,
+  AuthServiceClient,
+  ValidateResponse,
+} from './auth.pb';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  private svc: AuthServiceClient;
+
+  @Inject(AUTH_SERVICE_NAME)
+  private readonly client: ClientGrpc;
+
+  public onModuleInit(): void {
+    this.svc = this.client.getService<AuthServiceClient>(AUTH_SERVICE_NAME);
+  }
+
+  public async validate(token: string): Promise<ValidateResponse> {
+    return firstValueFrom(this.svc.validate({ token }));
+  }
+}
